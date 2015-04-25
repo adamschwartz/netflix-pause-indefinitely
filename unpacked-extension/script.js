2dd5e8decb774839c8624665ea9a43748a662d33
Adam Schwartz <aschwartz@hubspot.com>
2015-04-25 19:11:25 -0400

    Style improvements

--- unpacked-extension/script.js
+++ unpacked-extension/script.js
@@ -17,7 +17,7 @@
       document.body.innerHTML = '' +
         '<style>' +
           'body > div { background: #000; position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 9999999999; }' +
-          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 60px; width: 60px; border: 2px solid #f00; border-radius: 50%; border-left-color: transparent; border-top-color: transparent; -webkit-animation: kill-netflix-timeouts-spinner 1s linear infinite }' +
+          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 56px; width: 56px; border: 2px solid #f00; border-radius: 50%; border-left-color: transparent; border-top-color: transparent; -webkit-animation: kill-netflix-timeouts-spinner 1s linear infinite }' +
           '@-webkit-keyframes kill-netflix-timeouts-spinner { to { -webkit-transform: rotate(360deg) } }' +
         '</style>' +
         '<div><div></div></div>' +
@@ -32,11 +32,11 @@
 
   var showBetterMessageIfTimedOut = function() {
     if (isTimedOut()) {
-      document.body.innerHTML = ''
+      document.body.innerHTML = '' +
         '<style>' +
-          'body > div { background: red; position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 9999999999; }' +
-          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 60px; width: 60px; border: 4px solid #fff; border-radius: 50%; }' +
-          'body > div > div:before { content: ""; display: block; position: absolute; top: 0; right: 0; bottom: 0; left: 23px; margin: auto; height: 0px; width: 0px; border: 20px solid; border-color: transparent transparent transparent #fff; }' +
+          'body > div { background: #000; position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 9999999999; cursor: pointer; }' +
+          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 56px; width: 56px; border: 4px solid #fff; border-radius: 50%; }' +
+          'body > div > div:before { content: ""; display: block; position: absolute; top: 0; right: 0; bottom: 0; left: 22px; margin: auto; height: 0px; width: 0px; border: 17px solid; border-color: transparent transparent transparent #fff; }' +
         '</style>' +
         '<div><div></div></div>' +
       '';
