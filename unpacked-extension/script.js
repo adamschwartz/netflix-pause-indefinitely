3a8dee0ff140f1ad1358fd5103738702b7ab53d0
Adam Schwartz <aschwartz@hubspot.com>
2015-04-25 14:24:14 -0400

    Mimic Netflix’s spinner style

--- unpacked-extension/script.js
+++ unpacked-extension/script.js
@@ -17,7 +17,7 @@
       document.body.innerHTML = '' +
         '<style>' +
           'body > div { background: #000; position: fixed; top: 0; right: 0; bottom: 0; left: 0; z-index: 9999999999; }' +
-          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 30px; width: 30px; border: 2px solid #fff; border-radius: 50%; border-left-color: transparent; border-top-color: transparent; -webkit-animation: kill-netflix-timeouts-spinner 1s linear infinite }' +
+          'body > div > div { position: fixed; top: 0; right: 0; bottom: 0; left: 0; margin: auto; height: 60px; width: 60px; border: 2px solid #f00; border-radius: 50%; border-left-color: transparent; border-top-color: transparent; -webkit-animation: kill-netflix-timeouts-spinner 1s linear infinite }' +
           '@-webkit-keyframes kill-netflix-timeouts-spinner { to { -webkit-transform: rotate(360deg) } }' +
         '</style>' +
         '<div><div></div></div>' +
